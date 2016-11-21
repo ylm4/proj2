9cfdbdb5db370d21673e50a43c02066ec71147c5
Yanira Morales <ylm4@njit.edu>
2016-11-21 17:26:53 -0500

    html no exception

---
+++ htmlElementCreation.php
@@ -0,0 +1,45 @@
+<?php
+namespace HTMLelementCreation;
+class design{
+	public function table(){
+		return "<table>";
+	}
+	public function tTable(){
+		return "</table>";
+	}
+	public function rows(){
+		return "<tr></tr>";
+	}
+	public function elements($element){
+		foreach ($element as $elements){
+			echo "<td>{$elements}</td>";
+		}
+	}
+	public function tableRows($rows){
+		foreach ($rows as $row){
+			echo "<th>{$row}</th>";
+		}
+	}
+}
+$newDesign = new namespace\design;
+$row1= array("First Name", "Last Name", "Age");
+$row2=array("Yanira", "Morales", "21");
+$row3=array("John", "Stubs", "30");
+$row4= array("Abel", "Vega", "21");
+$row5= array("Wilbert","Roberts","19");
+
+echo $newDesign->table();
+echo $newDesign->rows();
+echo $newDesign->tableRows($row1);
+echo $newDesign->rows();
+echo $newDesign->elements($row2);
+echo $newDesign->rows();
+echo $newDesign->elements($row3);
+echo $newDesign->rows();
+echo $newDesign->elements($row4);
+echo $newDesign->rows();
+echo $newDesign->elements($row5);
+echo $newDesing->tTable();
+
+
+?>
